ac183c799d9e4e9863a2797a1aad533a799a3775
varixboi <varix24@gmail.com>
2026-08-18 00:41:46 +0530

    b2c-data products.json updated & price per piece added to return quote in b2c-estimator.ts

--- src/app/pages/b2c-estimator/b2c-estimator.ts
+++ src/app/pages/b2c-estimator/b2c-estimator.ts
@@ -205,9 +205,27 @@ export class B2cEstimator {
   }
 
   get Quote(){
+    if(this.shippingCost===0){
     return `${this.totalQty()} PCS of ${this.selectedProduct.product_name} (${this.selectedColor})
 Front: ${this.selectedFront.name}
 Back: ${this.selectedBack.name}
+
+PRICE PER PIECE: ₹${this.unitPrice}
+
+Net Weight: ${this.weightTotal} KG | Shipping Method: ${this.selectedShipping.courier_name}
+Tshirt total: ₹${this.subTotal[0]}
+
+TAXABLE: ₹${this.subTotal[0] + this.shippingCost}/- | GST: ₹${this.subTotal[1].toFixed(2)}/-
+FINAL TOTAL: ₹${this.subTotal[2].toFixed(2)}/-
+
+`;
+    }
+    else{
+    return `${this.totalQty()} PCS of ${this.selectedProduct.product_name} (${this.selectedColor})
+Front: ${this.selectedFront.name}
+Back: ${this.selectedBack.name}
+
+PRICE PER PIECE: ₹${this.unitPrice}
 
 Net Weight: ${this.weightTotal} KG
 Tshirt total: ₹${this.subTotal[0]} | Shipping cost: ₹${this.shippingCost}
@@ -216,6 +234,8 @@ Shipping Method: ${this.selectedShipping.courier_name} (${this.selectedShipping.
 TAXABLE: ₹${this.subTotal[0] + this.shippingCost}/- | GST: ₹${this.subTotal[1].toFixed(2)}/-
 FINAL TOTAL: ₹${this.subTotal[2].toFixed(2)}/-
 `;
+    }
+
   }
 
   copyQuote(){
